f2f898ae40d75629f995aa3955fcb50cfe1b68c9
Marco Giacomello <m.giacomello@insiel.it>
2022-12-29 16:06:28 +0100

    serizable #3

--- src/main/java/com/marco/dtocommoninterface/model/inventory/BeerInventoryDto.java
+++ src/main/java/com/marco/dtocommoninterface/model/inventory/BeerInventoryDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -12,7 +13,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class BeerInventoryDto {
+public class BeerInventoryDto implements Serializable {
     private UUID id;
     private OffsetDateTime createdDate;
     private OffsetDateTime lastModifiedDate;
